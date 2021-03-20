39c69943a10d0cc0cb0a01620317b9265406db7d
Nate Griggs <nateghsc09@gmail.com>
2021-03-20 17:03:20 -0500

    start of js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,27 @@
 
+$(document).ready(function () {
+    var searhHistoryContainer = $('#past-searches');
+    var searchForm = $('#search-form');
+    var apiKey = 'e7ef451762216c94d54aba8f75ac1dd2';
+    var baseUrl = 'api.openweathermap.org/data/2.5/weather?';
 
+    searchForm.submit(function (event) {
+        event.preventDefault();
+        console.log(event);
 
+        var formValues = $(this).serializeArray();
+        var city = formValues[0].value;
 
-//api key= e7ef451762216c94d54aba8f75ac1dd2
+        var searchTermDiv = $('<div class="past-search-term">');
+        searchTermDiv.text(city);
+        searhHistoryContainer.append(searchTermDiv);
+        console.log(formValues, city);
+
+        searchFormCityWeather(city);
+
+    });
+    function searchFormCityWeather(city) {
+        var fullUrl = baseUrl + "q=" + city ;
+        console.log(city);
+    }
+});
