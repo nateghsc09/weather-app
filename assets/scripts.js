29c10fc093633296482e834177334999b2ae8555
Nate Griggs <nateghsc09@gmail.com>
2021-03-20 17:33:09 -0500

    current is done

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,8 +2,9 @@
 $(document).ready(function () {
     var searhHistoryContainer = $('#past-searches');
     var searchForm = $('#search-form');
+    var currentWeatherContainer = $('#current-weather');
     var apiKey = 'e7ef451762216c94d54aba8f75ac1dd2';
-    var baseUrl = 'api.openweathermap.org/data/2.5/weather?';
+    var baseUrl = 'http://api.openweathermap.org/data/2.5/weather?';
 
     searchForm.submit(function (event) {
         event.preventDefault();
@@ -21,7 +22,42 @@ $(document).ready(function () {
 
     });
     function searchFormCityWeather(city) {
-        var fullUrl = baseUrl + "q=" + city ;
-        console.log(city);
+        var fullUrl = baseUrl + "q=" + city +"&appid=" + apiKey ;
+        console.log(fullUrl);
+        fetch(fullUrl).then(function(response) {
+            return response.json();
+
+        })
+        .then(function (data) {
+            console.log(data);
+            var cityName = data.name;
+            var temp = data.main.temp;
+            var humidity = data.main.humidity;
+            var weather = data.weather;
+            var wind = data.wind;
+            var cityNameDiv = $("<div class='city-name'>");
+            var tempDiv = $("<div class='temp-name'>");
+            var humidityDiv = $("<div class='humidity-name'>");
+            var weatherDiv = $("<div class='icon-name'>");
+            var windDiv = $("<div class='wind-name'>");
+            cityNameDiv.text(cityName);
+            tempDiv.text("Temperature: " + temp);
+            humidityDiv.text(" Humidity: " + humidity + " %" );
+            windDiv.text("Wind Speed: " + wind.speed + " MPH");
+
+            currentWeatherContainer.append(cityNameDiv);
+            currentWeatherContainer.append(tempDiv);
+            currentWeatherContainer.append(humidityDiv);
+            currentWeatherContainer.append(windDiv);
+            currentWeatherContainer.append(weatherDiv);
+           
+
+    
+            //currentWeatherContainer
+        });
+
+    }
+    function searchForFiveDayForcastWeather(city){
+
     }
 });
